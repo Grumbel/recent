70eb9052d06bc46a27dad5361fc5cea80f904669
Ingo Ruhnke <grumbel@gmail.com>
2023-03-17 22:10:04 +0100

    Add tinycmmc

--- flake.nix
+++ flake.nix
@@ -2,9 +2,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     flake-utils.url = "github:numtide/flake-utils";
+
+    tinycmmc.url = "github:grumbel/tinycmmc";
+    tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
+    tinycmmc.inputs.flake-utils.follows = "flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, tinycmmc }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -18,9 +22,24 @@
 
             src = ./.;
 
-            buildInputs = with pkgs; [
+            buildInputs = (with pkgs; [
               gtkmm3
               fmt_9
+
+              # Silence pkg-config warnings
+              pcre
+              pcre2
+              libthai
+              libdatrie
+              libxkbcommon
+              libepoxy
+              util-linux
+              libselinux
+              libsepol
+              xorg.libXdmcp
+              xorg.libXtst
+            ]) ++ [
+              tinycmmc.packages.${pkgs.system}.default
             ];
 
             nativeBuildInputs = with pkgs; [
